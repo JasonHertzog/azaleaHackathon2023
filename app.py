0ce0d8cddb99805667a60573149be7018c4aea0c
Jason Hertzog <jhertzog93@gmail.com>
2023-11-10 20:19:07 -0500

    WIP: Pivoted toward using data actually available, instead of data I thought would look good. I just don't have time to learn how to use Health Kit or whatever else. Back to drawing board.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import extra.helpers as helpers
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +8,22 @@ app = Flask(__name__)
 @app.route('/')
 def dashboard():
     return render_template('dashboard.html')
+
+
+@app.route('/get_patient_data/<patient_id>')
+def get_patient_data(patient_id):
+    fhir_server = 'https://app.azaleahealth.com/fhir/R4/sandbox'
+    headers = {
+        "Authorization": helpers.load_auth_token(),
+    }
+
+    response = requests.get(f'{fhir_server}/Patient/{patient_id}', headers=headers)
+
+    if response.status_code == 200:
+        patient_data = response.json()
+        return jsonify(patient_data)
+    else:
+        return jsonify({'error ' + str(response.status_code) + ': ': 'Patient not found!'})
 
 
 if __name__ == '__main__':
